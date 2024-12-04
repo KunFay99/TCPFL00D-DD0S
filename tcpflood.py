5bec4484b51f7af8f53e0a4de8710296a0738426
KunFay99 <kaffayukiffu@gmail.com>
2024-12-04 13:01:43 +0700

    Update tcpflood.py

--- tcpflood.py
+++ tcpflood.py
@@ -8,6 +8,23 @@ import progressbar
 
 # Colors
 class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    UNDERLINE = '\033[4m'
+    PURPLE = '\033[97m'
+    BOLD    = "\033[1m"
+    BLACK   = "\033[30m"
+    RED     = "\033[31m"
+    GREEN   = "\033[32m"
+    YELLOW  = "\033[33m"
+    BLUE    = "\033[34m"
+    MAGENTA = "\033[35m"
+    CYAN    = "\033[36m"
+    WHITE   = "\033[37m"
+
 # CLEAR
 os.system("clear")
 print(" ")
