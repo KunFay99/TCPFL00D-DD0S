d93c2afd387f87e48b1b2122abc4b816e4ad0207
KunFay99 <kaffayukiffu@gmail.com>
2025-10-04 23:06:15 +0700

    Update tcpflood.py

--- tcpflood.py
+++ tcpflood.py
@@ -32,29 +32,8 @@ class bcolors:
 
 # CLEAR
 os.system("clear")
-print(" ")
-print("\033[31m  © © ©© ©©        ©© © ©      ©© © ©          \033[0m")
-print("\033[31m      ©©         ©©            ©©     ©           \033[0m")
-print("\033[31m      ©©         ©©            ©©     ©           \033[0m")
-print("\033[33m      ©©         ©©            ©© © ©            \033[0m")
-print("\033[33m      @@           @@ @ @      @@             \033[0m")
-print("\033[33m      °°           °° ° °      °°            \033[0m")
-print("\033[33m       °              °        °              \033[0m")
-print("                                                                             ")   
-print("\033[32m   ©© © © ©   ©©            ©© ©           ©© ©       ©© © ©          \033[0m")
-print("\033[32m   ©©         ©©          ©©      ©     ©©      ©     ©©     ©        \033[0m")
-print("\033[32m   ©©         ©©         ©©        ©   ©©        ©    ©©      ©       \033[0m")
-print("\033[32m   ©© © © ©   ©©         ©©        ©   ©©        ©    ©©      ©       \033[0m")
-print("\033[32m   @@         @@          @@      @     @@      @     @@     @        \033[0m")
-print("\033[32m   @@         ®® @ @ @       @@ @          @@ @       @@ @ @          \033[0m")
-print("\033[32m   °°         °°  ° °        °°°           °° °       °° °            \033[0m")
-print("\033[32m   °           °  °           °             °          °°             \033[0m")
-print("\033[93m==================================≠==≠======≠≠============≠======= \033[0m")
-print("\033[93m           U S E   T H I S  S C R I P T  W I S E L Y               \033[0m")
-print("\033[93m                     Specialist attack TCP                         \033[0m")
-print("\033[93m                           By: ZA'99                               \033[0m")
-print("\033[93m                           ——oO0Oo——                               \033[0m")
-print("\033[93m==================================================================\033[0m")
+print("""
+""")
 print("\033[94m————————————————————————⟩⟩⟩")
 ip = str(input("\033[93m[\033[93m+\033[92m]                      ⟩⟩ IP Target : "))
 print("\033[94m————————————————————————⟩⟩⟩")
@@ -98,9 +77,6 @@ def start():
         u += 1
         print("\033[92m[" +str(u)+ "]  \033[32m[FOOODING WEBS]  \033[36mSent attack  \033[97mTarget: [" +ip+ "]  \033[0m")
         print("\033[93m[" +str(u)+ "]  \033[33m[FOOODING WEBS]  \033[96mSent attack  \033[92mTarget: [" +ip+ "]  \033[0m")
-        print("\033[94m[" +str(u)+ "]  \033[34m[FOOODING WEBS]  \033[36mSent attack  \033[93mTarget: [" +ip+ "]  \033[0m")
-        print("\033[95m[" +str(u)+ "]  \033[35m[FOOODING WEBS]  \033[93mSent attack  \033[36mTarget: [" +ip+ "]  \033[0m")
-        print("\033[96m[" +str(u)+ "]  \033[36m[FOOODING WEBS]  \033[37mSent attack  \033[31mTarget: [" +ip+ "]  \033[0m")
         
     except:
         s.close()
